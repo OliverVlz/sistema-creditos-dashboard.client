5a6d3b095f3dfcf51a19863eb7f50fa01e8ad201
Oliver <Oliver.vlz01@gmail.com>
2025-12-15 08:21:52 -0500

    fix: corregir tipos de dispatch para React 19 y eliminar archivos pnpm

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,5 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
-import type { ThunkAction, AnyAction } from "@reduxjs/toolkit";
+import { configureStore, ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
+import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import usersReducer from "../features/user-management/slices/users.slices";
 import clientsReducer from "../features/customer-management/slices/client.slices";
 import organizationsReducer from "../features/customer-management/slices/organizationsSlice";
@@ -24,13 +23,7 @@ export const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  AnyAction
->;
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>;
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
